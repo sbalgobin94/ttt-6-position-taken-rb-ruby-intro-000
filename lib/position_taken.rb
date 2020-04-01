70a76d8c7139e45afb942433c9e1b12359ab5642
Samantha Balgobin <sbalgobin94@gmail.com>
2020-04-01 15:43:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,9 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "  " || board[index] == nil
     return false
   end
-  
+
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+
 end
